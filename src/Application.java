e05e9fad5d1a68ac2406b1934fec8b0e6b8247fb
Jorge Ferreiro <me@jgferreiro.com>
2016-01-10 04:27:05 +0100

    Adding Application main

---
+++ src/Application.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Application {
+	public static void main(String args[]) {
+		
+		int n = -1;
+		boolean correct = false;
+		BTree myTree;
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Hi Teacher! Welcome to my program!");
+		
+		do {
+			
+			System.out.println("Introduce the n-size for the B-Tree: ");
+			n = in.nextInt();
+			
+			if ( n >= Constants.MINIMUM_KEYS )
+				correct = true;
+			else
+				System.err.println("Hey! The B-tree size must be greater than 2!");
+			
+		} while ( ! correct );
+		
+		myTree = new BTree( n );
+		
+		// ToDO
+		// Insert...
+		
+	}
+}
